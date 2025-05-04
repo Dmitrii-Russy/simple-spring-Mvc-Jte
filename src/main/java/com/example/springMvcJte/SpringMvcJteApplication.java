a618747bff43b3bb62407920b1c3c4fce10fd1a4
Dmitrii Russu <dmitriirussulink@.gmail.com>
2025-05-04 22:07:26 +0300

    Первый коммит: добавлен spring boot проект

---
+++ src/main/java/com/example/springMvcJte/SpringMvcJteApplication.java
@@ -0,0 +1,12 @@
+package com.example.springMvcJte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcJteApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcJteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springMvcJte/SpringMvcJteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springMvcJte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcJteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
